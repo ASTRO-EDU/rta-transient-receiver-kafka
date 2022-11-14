056ed7bf1d29c5b21cddbc1f9fffa84affb071f8
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-11-14 16:45:11 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="voeventhandler",
-    author="Antonio Addis, Luca Babboni",
-    version="2.0.0",
+    name="kafkareceiver",
+    author="Luca Babboni, Antonio Addis",
+    version="1.0.0",
     packages=find_packages(),
-    package_dir={ 'voeventhandler': 'voeventhandler'},
+    package_dir={ 'kafkareceiver': 'kafkareceiver'},
     include_package_data= True,
     license='GPL-3.0'
 )
